892128fec97117a2158e722f85b4a3de893d5dfb
shivakarka <shivapro01@hotmail.com>
2023-07-03 17:27:06 +0530

    created fetch api endpoint and basic html template

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
+const dotenv = require("dotenv");
+dotenv.config();
+const fetch = require("node-fetch");
 
 const app = express();
 const cors = require("cors");
@@ -15,11 +18,23 @@ app.get("/", function (req, res) {
   //   res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
+app.get("/api", function (req, res) {
+  const url = req.body.url;
+  const key = process.env.API_KEY;
+  const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
+  const apiURL = `${baseURL}?key=${key}&url=${url}&lang=en`;
+  fetch(apiURL)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send(data);
+    });
+});
+
 // designates what port the app will listen to for incoming requests
 app.listen(8082, function () {
   console.log("Example app listening on port 8082!");
 });
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
-});
+// app.get("/test", function (req, res) {
+//   res.send(mockAPIResponse);
+// });
